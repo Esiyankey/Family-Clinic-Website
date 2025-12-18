5730f561273d16412c5bb8dd5d568e55e9e59164
Grace Yankey <graceyan244@gmail.com>
2025-12-18 10:36:38 +0000

    update patient satisfaction statistic to 95% and add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
+   icons: {
+    icon: "/favicon.png",
+  },
   title: "Family Clinic",
   description:
     "Welcome to Family Clinic your trusted partner in health and wellness",
